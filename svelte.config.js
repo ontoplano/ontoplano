93c2fcb5261f25dd58685307e394fc597e52aa3d
omo <omo@chedieck.com>
2026-04-14 14:59:37 -0300

    fix: disable CSRF origin check for multi-origin LAN access (#67)

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,12 @@ const config = {
 			return isExternalLibrary ? undefined : true;
 		}
 	},
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false
+		}
+	},
 	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
 	extensions: ['.svelte', '.svx', '.md']
 };
